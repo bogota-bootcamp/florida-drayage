260808f70cc86b7434d5c68ce9db592e42b4d804
Jose Medina <fermed28@gmail.com>
2018-07-10 14:59:12 -0500

    generate chanel and create action cable conection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,8 +2,8 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
   	identified_by :uuid
 
-  	def connect
-  		self.uuid = current_user.id
+  	def connect  		
+  		self.uuid = cookies[:user_id] if cookies[:user_id]
   	end
   end
 end
