c1775615d2d81bbd0d8c4821ee23cac669d43a9a
Jose Medina <fermed28@gmail.com>
2018-07-10 21:59:50 -0500

    add simple model of Message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_154637) do
+ActiveRecord::Schema.define(version: 2018_07_11_025112) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
