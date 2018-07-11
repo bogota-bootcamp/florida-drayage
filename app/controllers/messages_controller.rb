fb0811493abb7c894617e74105d7fc555178a449
Jose Medina <fermed28@gmail.com>
2018-07-10 23:14:09 -0500

    Create Message and list with ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,14 @@
 class MessagesController < ApplicationController
 
 	def create
+		@message = Message.create(message_params)
+		respond_to do |format|
+			format.js
+		end
+	end
+
+	private
+	def message_params
+		params.require(:message).permit(:body, :user_id)
 	end
 end
