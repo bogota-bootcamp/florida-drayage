a1faacb8faf24e0edb391d751ee60f0ada0be973
Jose Medina <fermed28@gmail.com>
2018-07-11 16:46:30 -0500

    set chanels to admin and client fist simple version of chat

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,8 +2,13 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
   	identified_by :uuid
 
-  	def connect  		
-  		self.uuid = cookies[:user_id] if cookies[:user_id]
+  	def connect
+  		user=User.find(cookies[:user_id])
+  		if user.has_role? :admin
+  			self.uuid = "admin" 
+  		else
+  			self.uuid = cookies[:user_id] 
+  		end
   	end
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,17 @@
 class MessagesController < ApplicationController
 
 	def create
+		puts '*'*50
+		puts params
 		@message = Message.create(message_params)
 		response = render :partial => "messages/message"
-		ActionCable.server.broadcast "user_#{current_user.id}", {action: "created", msg: response	 }
-		
+		#creates 
+	  if current_user.has_role? :admin and params[:client]
+			ActionCable.server.broadcast "user_#{params[:client][:user_id]}", {action: "created", msg: response	 }
+		else	
+			ActionCable.server.broadcast "user_#{current_user.id}", {action: "created", msg: response	 }
+		end
+		ActionCable.server.broadcast "user_admin", {action: "created", msg: response	 }
 	end
 
 	private
