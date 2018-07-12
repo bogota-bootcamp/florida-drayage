f684b2134ae31bb6e5bf94781b8b65c3c81feb1d
Jose Medina <fermed28@gmail.com>
2018-07-12 10:47:24 -0500

    change model messages and create model conversation

--- db/migrate/20180712154611_create_messages.rb
+++ db/migrate/20180712154611_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.text :body
-      t.references :user, foreign_key: true
+      t.references :conversation, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_025112) do
+ActiveRecord::Schema.define(version: 2018_07_12_154611) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.string "name"
+    t.string "mail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
-    t.integer "user_id"
+    t.integer "conversation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_messages_on_user_id"
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
   end
 
   create_table "roles", force: :cascade do |t|
