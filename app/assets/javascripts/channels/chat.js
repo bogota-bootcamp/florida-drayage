d686194a98e845ced7a42cf63c4a5d459e0f8d97
diegozwolf <diegozuluaga88@gmail.com>
2018-07-23 15:44:36 -0500

    fix font -size in user card

--- app/assets/javascripts/channels/chat.js
+++ app/assets/javascripts/channels/chat.js
@@ -21,7 +21,7 @@
                     <i class="material-icons user-icon">person_pin</i>
                   </div>
                   <div class="col-7 col-height padding-0 d-flex flex-column justify-content-center align-items-user">
-                    <p class="client-name"><a class="go-chat" data-remote="true" href="/conversations/`+data.conversation.id+`">`+data.conversation.id+`-`+data.conversation.name+`</a></p>
+                    <p class="client-name client-border"><a class="client-name" data-remote="true" href="/conversations/`+data.conversation.id+`">`+data.conversation.id+`-`+data.conversation.name+`</a></p>
                     <p class="client-mail">`+data.conversation.mail+`</p>
                   </div>
                   <div class="col-3 col-height padding-0 flex-b">
