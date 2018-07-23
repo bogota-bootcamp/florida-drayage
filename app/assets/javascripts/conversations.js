edfb1e18c7d1d74081a6fff31a7aaecda68df429
Jose Medina <fermed28@gmail.com>
2018-07-23 11:13:09 -0500

    #55 add onclick card event to render conversation

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,3 +1,4 @@
 /*Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/ */
+$("#cart-container").on("click","li",function(e){ $(this).find( "a" )[0].click();})
