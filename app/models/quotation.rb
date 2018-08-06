53875c7fa724a9e65bca2d7c2cab888ee61ea755
Jose Medina <fermed28@gmail.com>
2018-08-06 15:57:41 -0500

    add class method to obtain zip code object (origin y destination)

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,10 +1,20 @@
 class Quotation < ApplicationRecord
 	has_many :invoices
+	
 	validates :first_name, :presence => true
 	validates :last_name, :presence => true
 	validates :company, :presence => true
 	validates :phone, :presence => true
 	validates :email, :presence => true
 	validates :comments, :presence => true
+	
+
+	def origin_zipcode_object
+		 zipcode= Zipcode.find(self.origin_zipcode) if self.origin_zipcode
+	end	
+
+	def destination_zipcode_object
+		zipcode= Zipcode.find(self.destination_zipcode) if self.destination_zipcode
+	end
 end
 	
