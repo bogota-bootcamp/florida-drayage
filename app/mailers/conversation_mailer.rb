ec7c2e9a294d55bf8696d85647a017f4ec2db2a2
Jose Medina <fermed28@gmail.com>
2018-08-14 15:13:20 -0500

    add mailer when Quote was created, the Email will be send to email stored in the form

--- app/mailers/conversation_mailer.rb
+++ app/mailers/conversation_mailer.rb
@@ -1,8 +1,8 @@
 class ConversationMailer < ApplicationMailer
   def new_conversation(conversation)
-  	
-  	@conversation= conversation  	
-  	mail(to: "fermed28@gmail.com",subject: 'New conversation',
-  		delivery_method_options: { api_key: ENV['Mailjet_api_key'], secret_key: ENV['Mailjet_secret_key'] })
+    
+    @conversation= conversation   
+    mail(to: "fermed28@gmail.com",subject: 'New conversation',
+      delivery_method_options: { api_key: ENV['Mailjet_api_key'], secret_key: ENV['Mailjet_secret_key'] })
   end
 end
