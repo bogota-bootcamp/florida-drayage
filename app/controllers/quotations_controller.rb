64919568660951e3b27cc0dfd9aabfd93e2d6059
Faiber Rodriguez <greatfaiber@gmail.com>
2019-05-09 14:45:43 -0500

    dropdown added with logic, also on invoices, icon missing

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -73,6 +73,6 @@ class QuotationsController < ApplicationController
 
   private
   def quotation_parameters
-    params.require(:quotation).permit(:first_name,:last_name,:company,:phone,:email,:comments,:commodity,:hazardous,:bonded_cargo,:overweight,:pickup_date,:drop_date,:equipment_type,:origin_zipcode,:destination_zipcode,:origin_city,:destination_city,:residencial,:export)
+    params.require(:quotation).permit(:first_name,:last_name,:company,:phone,:email,:comments,:commodity,:hazardous,:bonded_cargo,:overweight,:pickup_date,:drop_date,:equipment_type,:additional_service, :origin_zipcode,:destination_zipcode,:origin_city,:destination_city,:residencial,:export)
   end
 end
